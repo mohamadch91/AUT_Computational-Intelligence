6fbd09fa4c789833ad8b4a9578f167e51ad9942a
Mohamad Choupan <mohamadkhoee@yahoo.com>
2022-01-22 01:17:58 +0330

    implement weights and biass
    also define activation function sigmoid

--- project3/SnailJumper-master/nn.py
+++ project3/SnailJumper-master/nn.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 
 class NeuralNetwork:
 
@@ -10,8 +10,14 @@ class NeuralNetwork:
         :param layer_sizes: A list containing neuron numbers in each layers. For example [3, 10, 2] means that there are
         3 neurons in the input layer, 10 neurons in the hidden layer, and 2 neurons in the output layer.
         """
-        # TODO (Implement FCNNs architecture here)
-        pass
+
+        np.random.seed(1)
+        self.w=[]
+        self.b=[]
+        for i in range(len(layer_sizes)-1):
+            self.w.append (np.random.normal(size=(layer_sizes[i+1],layer_sizes[i] )))
+            self.b.append(np.zeros((layer_sizes[i+1], 1)))
+
 
     def activation(self, x):
         """
@@ -19,8 +25,12 @@ class NeuralNetwork:
         :param x: Vector of a layer in our network.
         :return: Vector after applying activation function.
         """
-        # TODO (Implement activation function here)
-        pass
+        new_x = []
+        for i in range(len(x)):
+            z = 1 / (1 + np.exp(-x[i]))
+            new_x.append(z)
+        return new_x
+
 
     def forward(self, x):
         """
